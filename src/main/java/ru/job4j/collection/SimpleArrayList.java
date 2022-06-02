f0718bcbc5620b1efdc7d5c2e31bc83cc5e473c6
Sagkar <Sagkar.savaie@yandex.ru>
2022-06-02 16:15:53 +0300

    Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -0,0 +1,82 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+
+    private T[] container;
+
+    private int size = 0;
+
+    private int modCount = 0;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        container[size++] = value;
+        iterator().next();
+        modCount++;
+        if (size >= container.length) {
+            increase();
+        }
+    }
+
+    @Override
+    public T set(int index, int newValue) {
+        Objects.checkIndex(index, size);
+        T old = get(index);
+        container[index] = (T) (Integer) newValue;
+        return old;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, size);
+        T element = get(index);
+        System.arraycopy(container, index + 1, container, index, container.length - index - 1);
+        container[container.length - 1] = null;
+        size--;
+        modCount++;
+        return element;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int expectedModCount = modCount;
+            int elNum = 0;
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return elNum < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return container[elNum++];
+            }
+        };
+    }
+    public void increase() {
+        container = Arrays.copyOf(container, container.length * 2);
+    }
+}
